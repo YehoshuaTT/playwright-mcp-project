da8035aa72a259880c496742b254af327cd72f90
Josh <a0526741852@gmail.com>
2025-06-29 01:46:27 +0300

    .

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3500;
 
 // Middleware
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '51mb' }));
 
 // Global variables
 let browser = null;
